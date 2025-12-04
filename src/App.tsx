c987b3fb3763b210c375802d45f07cdd08fb4af0
DainisFyodorov <fedorovdainis@gmail.com>
2025-12-05 00:34:10 +0200

    Review List Page routing implemented

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 import { Auth0Provider, withAuthenticationRequired } from '@auth0/auth0-react';
 import { auth0Config } from './lib/auth0Config';
 import LoginPage from './Auth/LoginPage';
+import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
 
 const Auth0ProviderWithHistory = ({ children }: { children: React.ReactNode }) => {
   const history = useHistory();
@@ -54,6 +55,9 @@ export const App = () => {
             </Route>
             <Route path='/search'>
               <SearchBooksPage />
+            </Route>
+            <Route path='/reviewlist/:bookId'>
+              <ReviewListPage />
             </Route>
             <Route path='/checkout/:bookId'>
               <BookCheckoutPage />
