d7d83b680f5dc65284e707653c1c302d982c9bf0
DainisFyodorov <fedorovdainis@gmail.com>
2025-12-11 00:20:07 +0200

    LoansModal implemented

--- src/layouts/ShelfPage/components/Loans.tsx
+++ src/layouts/ShelfPage/components/Loans.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 import { Link } from "react-router-dom";
+import { LoansModal } from "./LoansModal";
 
 export const Loans = () => {
     
@@ -117,6 +118,7 @@ export const Loans = () => {
                                     </div>
                                 </div>
                                 <hr />
+                                <LoansModal shelfCurrentLoan={shelfCurrentLoan} mobile={false} />
                             </div>
                         ))}
                     </>
@@ -187,6 +189,7 @@ export const Loans = () => {
                                     </div>
                                 </div>
                                 <hr />
+                                <LoansModal shelfCurrentLoan={shelfCurrentLoan} mobile={true} />
                             </div>
                         ))}
                     </>
