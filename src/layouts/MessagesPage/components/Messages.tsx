894a37f97a54c723a1d65e64a5894172d2d0d1af
DainisFyodorov <fedorovdainis@gmail.com>
2025-12-27 20:10:54 +0200

    Messages component implemented

---
+++ src/layouts/MessagesPage/components/Messages.tsx
@@ -0,0 +1,50 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
+import MessageModel from "../../../models/MessageModel";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+
+export const Messages = () => {
+    
+    const { isAuthenticated, getAccessTokenSilently, user } = useAuth0();
+    const [isLoadingMessages, setIsLoadingMessages] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    // Messages
+    const [messages, setMessages] = useState<MessageModel[]>([]);
+
+    // Pagination
+    const [messagesPerPage] = useState(5);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
+
+    useEffect(() => {
+        const fetchUserMessages = async () => {
+
+        }
+        fetchUserMessages().catch((error: any) => {
+            setIsLoadingMessages(false);
+            setHttpError(error.message);
+        })
+        window.scrollTo(0, 0);
+    }, [isAuthenticated, currentPage]);
+
+    if(isLoadingMessages) {
+        return (
+            <SpinnerLoading />
+        );
+    }
+
+    if(httpError) {
+        return (
+            <div className="container m-5">
+                <p>{httpError}</p>
+            </div>
+        );
+    }
+
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+    return (
+        <div></div>
+    );
+}
