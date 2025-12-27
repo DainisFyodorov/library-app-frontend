c1b6620b88105b76f2796f8f64676efbba705da4
DainisFyodorov <fedorovdainis@gmail.com>
2025-12-27 22:33:47 +0200

    Secure Route added for manage library page

--- src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
+++ src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
@@ -43,11 +43,11 @@ export const ManageLibraryPage = () => {
                     </div>
                     <div className="tab-pane fade" id="nav-quantity" role="tabpanel"
                         aria-labelledby="nav-quantity-tab">
-                            Change quantity
+                            Change Quantity
                     </div>
                     <div className="tab-pane fade" id="nav-messages" role="tabpanel"
                         aria-labelledby="nav-messages-tab">
-                            Messages
+                            Admin Messages
                     </div>
                 </div>
             </div>
